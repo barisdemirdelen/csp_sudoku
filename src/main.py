57066b0f9bf2c831b9eae39adb28ad49966ccbe9
MBleeker <maurits.bleeker@nubisonline.nl>
2015-09-30 09:16:49 +0200

    added last constraints

--- src/main.py
+++ src/main.py
@@ -28,24 +28,44 @@ def make_variables(sudokus):
 def created_sudoku_constraints(variables):
     constaints_array = []
     constraint_num = 1
+    # get all the variables per row
     for i in range(0, 82, 9):
         constaints_array.append(AllDifferentConstraint("name%d" % constraint_num, variables[i:i + 9]))
         constraint_num += 1
         if i + 9 >= 81:
             break
+    # get all the variables per column
     for colom in range(0, 9):
         colom_vars = []
         for cell in range(colom, 81, 9):
             colom_vars.append(variables[cell])
         constaints_array.append(AllDifferentConstraint("name%d" % constraint_num, colom_vars))
         constraint_num += 1
-    return constaints_array
+    # quit a complected loop to get the indexes of every block
+    # maybe we van split up this code or rewrite it, for now it works 
+    # get all the variables per block
+    row_start = 0
+    row_end = 26
+    for row_block in range(0,3):
+        column_start = 0
+        column_end = 2
+        for column_block in range(0,3):
+            block_vars = []
+            for i in range(0,82):
+                if ((i%9 >= column_start and i%9 <= column_end) and ( i >= row_start and i <= row_end)):
+                    block_vars.append(variables[i])
+            constaints_array.append(AllDifferentConstraint("name%d" % constraint_num, block_vars))
+            constraint_num += 1
+            column_start += 3
+            column_end += 3
+        row_start += 27
+        row_end += 27
+        return constaints_array
 
 
 
 sudokus = read_sudoku()
 variables = make_variables(sudokus)
-
 constraints = created_sudoku_constraints(variables)
 
 start = time.time()
@@ -53,7 +73,6 @@ cp = CP()
 cp.variables = variables
 cp.constraints = constraints
 assignment = cp.search()
->>>>>>> 7186d3b7f83594a058ac91fa958a0ea9de262676
 
 i = 1
 for variable in cp.variables:
@@ -67,21 +86,4 @@ end = time.time()
 print "runtime:", end - start, "seconds."
 
 
-'''
-row_start = 0
-row_end = 26
-for row_block in range(0,3):
-    column_start = 0
-    column_end = 2
-    for column_block in range(0,3):
-        array = []
-        for i in range(0,82):
-            if ((i%9 >= column_start and i%9 <= column_end) and ( i >= row_start and i <= row_end)):
-                array.append(i)
-        print array  
-        column_start += 3
-        column_end += 3
-    row_start += 26
-    row_end += 26
 
-'''
